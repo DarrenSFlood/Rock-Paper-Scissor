0fc9a22c8068530d22a03fb7aa0f191c0bbcf787
Darren <Darren.S.Flood@icloud.com>
2024-11-20 14:42:50 -0500

    feat: Add single round function
    Added arrow function for playround. Used switch statement because I
    thought it looked cleaner. It should update the global score variables.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,14 +1,18 @@
+//global scope player score
+let humanScore = 0;
+let computerScore = 0;
+
 //function to randomly generate a Rock, Paper, or Scissor value for the PC
 
 let getComputerChoice = function () {
    let randomNumber = Math.random();
 
     if (randomNumber >= 0.66){
-        return "Scissor";
+        return "scissor";
     } else if (randomNumber >= 0.33) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Rock";
+        return "rock";
     }
 }
 
@@ -19,8 +23,54 @@ let getHumanChoice = function() {
     return playerChoice;
 }
 
-//global scope player score
-let humanScore = 0;
-let computerScore = 0;
 
+// Function to play a single round. This should update the global scores and provide a console message stating the outcome.
+let playRound = (humanChoice, computerChoice) => {
+    let victoryMessage = `You win! ${humanChoice} beats ${computerChoice}!`;
+    let defeatMessage = `You lose! ${computerChoice} beats ${humanChoice}!`;
+    let tieMessage = `Tie! Try again!`;
+
+    if (humanChoice === 'rock') {
+        switch (computerChoice) {
+            case 'rock':
+                console.log(tieMessage);
+                break;
+            case 'paper':
+                console.log(defeatMessage);
+                break;
+            case 'scissor':
+                console.log(victoryMessage);
+                break;
+        }
+    } else if (humanChoice === 'paper') {
+        switch (computerChoice) {
+            case 'paper':
+                console.log(tieMessage);
+                break;
+            case 'rock':
+                console.log(victoryMessage);
+                break;
+            case 'scissor':
+                console.log(defeatMessage);
+                break;
+        }
+    } else {
+            switch (computerChoice) {
+                case 'scissor':
+                    console.log(tieMessage);
+                    break;
+                case 'rock':
+                    console.log(defeatMessage);
+                    break;
+                case 'paper':
+                    console.log(victoryMessage);
+                    break;
+            }
+        }
+ }
+
+
+
+
+playRound(getHumanChoice(), getComputerChoice());
 
